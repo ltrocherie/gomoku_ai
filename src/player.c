971476a31a7b0ed1ba543ceb35204b8d4c1703d0
Trocherie Lucas <Lucas.Trocherie@bordeaux-inp.fr>
2019-03-15 15:48:49 +0100

    Ajout des deux premières fonctions sur player.c

--- src/player.c
+++ src/player.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_NAME_LENGTH
+#include "player.h"
+
+#define MAX_NAME_LENGTH 10
+#define NB_OPENING_MOVE 3
 
 char const* get_player_name(){
-  char const name[MAX_NAME_LENGTH] = malloc(MAX_NAME_LENGTH * sizeof(char));
+  char* name = malloc(MAX_NAME_LENGTH*sizeof(char));
   scanf("[a-zA-Z0-9 -_]%s", name);
   return name;
 };
 
+struct col_move_t* propose_opening(size_t size){
+  struct col_move_t* move = malloc(NB_OPENING_MOVE * sizeof(struct col_move_t));
+  for(int i = 0; i<NB_OPENING_MOVE; i++){
+    struct move_t mv;
+    mv.row = i;
+    mv.col = i;
+    struct col_move_t mv_next;
+    mv_next.m = mv;
+    mv_next.c = i%2;
+    move[i] = mv_next;
+  }
+  return move;
+};
