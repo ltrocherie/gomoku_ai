576a7c4924612923e8ece4cdc7b5ee22f95ea14d
Ouedraogo Sonia De Laeticia <Sonia_De_Laeticia.Ouedraogo@bordeaux-inp.fr>
2019-03-29 11:42:04 +0100

    ajout de move.c

---
+++ src/move.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "move.h"
+#include "game.h"
+
+/*
+ */
+void enqueue(struct col_move_t m, struct col_move_t moves[], size_t* n_moves)
+{
+  if(*n_moves != NB_PLAYERS && *n_moves != 2*NB_PLAYERS)
+    {
+      moves[*n_moves] = m;
+      *n_moves += 1;
+    }
+  else
+    {
+      for(int i=0; i<NB_PLAYERS-1;i++)
+	{
+	  moves[i] = moves[*n_moves-NB_PLAYERS+1+i];
+	  moves[NB_PLAYERS-1] = m;
+	  *n_moves = NB_PLAYERS;
+	}
+    }
+}
+
