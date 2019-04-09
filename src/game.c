5750b8d2d8984ece4cfc02cb0014f938b2e84f1d
Pavia Pierre <Pierre.Pavia@bordeaux-inp.fr>
2019-04-09 14:48:45 +0200

    détails

--- src/game.c
+++ src/game.c
@@ -4,11 +4,11 @@
 #include "move.h"
 
 //renvoie l'entier associé au gagnant s'il y en a un apres le move cm
-//renvoie l'entier associé au perdant + 4 s'il y en a un après le move cm 
+//renvoie l'entier associé au perdant + 4 s'il y en a un après le move cm
 //renvoie -1 s'il n'y a ni gagnant ni perdant
 int is_winning(struct board bd, struct col_move_t cm)
 {
-  if (align(bd, cm) == 5)
+  if (align(bd, cm) >= 5)
     return cm.c;
   //  if (align(bd, cm) > 5)
   //return cm.c+4;
